c3f0a570e7c3bc1b3cae6742a810be9133341d9d
Okuhle charlieman <okuhlecharlieman@gmail.com>
2024-07-08 14:29:51 +0200

    Backend + UI|Express init + Signup page

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const app = require("./app");
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server for handiling uncaughtException`);
+});
+
+//config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "config/.env" });
+  console.log(".env activated!!!");
+}
+
+//Create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server for handiling unhandledRejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
